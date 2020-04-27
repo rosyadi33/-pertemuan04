bde3f28938f3648714ec31ab27756f7f5337cf58
ACER <mrosyadi23@outlook.com>
2020-04-27 15:14:00 +0800

    belajar Array

---
+++ src/Array/array.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class array {
+    public static void main(String[] args) {
+        int nilaiArr[] = {40,55,63,17,22,68,89,79,89};
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[8]);
+    }
+}
